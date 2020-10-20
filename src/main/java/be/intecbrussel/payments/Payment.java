3c8c20c6b6b637902be2058e6336a616db1a884c
Franck Kamenan <franck.kamenan@yahoo.com>
2020-10-20 13:14:35 +0200

    payments table

---
+++ src/main/java/be/intecbrussel/payments/Payment.java
@@ -0,0 +1,57 @@
+package be.intecbrussel.payments;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    private int customerNumber;
+    @Id
+    private String checkNumber;
+    private LocalDate paymentDate;
+    private double amount;
+
+    public Payment() {}
+
+    public Payment(int customerNumber, String checkNumber, LocalDate paymentDate, double amount) {
+        this.customerNumber = customerNumber;
+        this.checkNumber = checkNumber;
+        this.paymentDate = paymentDate;
+        this.amount = amount;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCheckNumber() {
+        return checkNumber;
+    }
+
+    public void setCheckNumber(String checkNumber) {
+        this.checkNumber = checkNumber;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
