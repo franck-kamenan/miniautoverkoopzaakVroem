91a44c2bbe1f0e008203cf833f73a99c9b81af88
Franck Kamenan <franck.kamenan@yahoo.com>
2020-10-20 13:31:09 +0200

    all tables

---
+++ src/main/java/be/intecbrussel/products/Product.java
@@ -0,0 +1,107 @@
+package be.intecbrussel.products;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    private String productCode;
+    private String productName;
+    private String productLine;
+    private String productScale;
+    private String productVendor;
+    private String productDescription;
+    private int quantityStock;
+    private double buyPrice;
+    private double MSRP;
+
+    public Product() {}
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductLine() {
+        return productLine;
+    }
+
+    public void setProductLine(String productLine) {
+        this.productLine = productLine;
+    }
+
+    public String getProductScale() {
+        return productScale;
+    }
+
+    public void setProductScale(String productScale) {
+        this.productScale = productScale;
+    }
+
+    public String getProductVendor() {
+        return productVendor;
+    }
+
+    public void setProductVendor(String productVendor) {
+        this.productVendor = productVendor;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public int getQuantityStock() {
+        return quantityStock;
+    }
+
+    public void setQuantityStock(int quantityStock) {
+        this.quantityStock = quantityStock;
+    }
+
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(double buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public double getMSRP() {
+        return MSRP;
+    }
+
+    public void setMSRP(double MSRP) {
+        this.MSRP = MSRP;
+    }
+
+    public Product(String productCode, String productName, String productLine, String productScale, String productVendor, String productDescription, int quantityStock, double buyPrice, double MSRP) {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.productLine = productLine;
+        this.productScale = productScale;
+        this.productVendor = productVendor;
+        this.productDescription = productDescription;
+        this.quantityStock = quantityStock;
+        this.buyPrice = buyPrice;
+        this.MSRP = MSRP;
+
+
+    }
+}
